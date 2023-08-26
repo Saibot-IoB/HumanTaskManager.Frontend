9924875ff87327de8b393cde2bc4c0469f07753b
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-08-26 12:40:11 +0200

    Initial commit

--- App.tsx
+++ App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const name: any = 'John Doe';
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
