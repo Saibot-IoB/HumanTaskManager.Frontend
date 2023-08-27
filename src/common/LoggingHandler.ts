cb47a211b8ba96ff52de85950b33672449d7ee6f
Tobias Kristensen <tobiaskristensen12@gmail.com>
2023-08-27 19:14:48 +0200

    feat: Add basic logging

---
+++ src/common/LoggingHandler.ts
@@ -0,0 +1,32 @@
+﻿/** TODO:
+ * Add Remote logging by making a call to the backend API.
+ * The backend API will then send the log to Logz.io via the secret token.
+ */
+
+export default class LoggingHandler {
+    public static info(message: string) {
+        console.info(`${new Date().toISOString()}` + message);
+        // fetch(`https://listener.logz.io:8071?token=${process.env.LOGZIO_TOKEN}`, {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         message: `${new Date().toISOString()}` + message,
+        //         level: 'info',
+        //     }),
+        // }).then(() => {
+        //     // Do nothing
+        // });
+    }
+
+    public static error(message: string) {
+        console.error(`${new Date().toISOString()}` + message);
+        // fetch(`https://listener.logz.io:8071?token=${process.env.LOGZIO_TOKEN}`, {
+        //     method: 'POST',
+        //     body: JSON.stringify({
+        //         message: `${new Date().toISOString()}` + message,
+        //         level: 'error',
+        //     }),
+        // }).then(() => {
+        //     // Do nothing
+        // });
+    }
+}
